bf838906a2aaf1ba619f6dd91af13672d3c5896c
Lena <ruth@bigmir.net>
2017-11-19 23:03:03 +0200

    page scroll hidden, burger components visible, fullscreen menu up

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ let menuMobile = document.querySelector('.hamburger-menu')
 let menuClose = document.querySelector('.hamburger-menu__close-btn')
 
 menuButton.addEventListener('click', () => {
-    menuMobile.classList.add('hamburger-menu_visible')   
+    menuMobile.classList.add('hamburger-menu_visible')
+    document.body.classList.add("no-scroll");
 })
 menuClose.addEventListener('click', () => {
     menuMobile.classList.remove('hamburger-menu_visible')   
+    document.body.classList.remove("no-scroll");   
 })
